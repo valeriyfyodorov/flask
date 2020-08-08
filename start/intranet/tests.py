a229314380395366ee066e9775544da469e6929e
Valeriy Fyodorov <valeriy.fyodorov@gmail.com>
2020-08-08 09:42:29 +0300

    New test.py file

--- start/intranet/tests.py
+++ start/intranet/tests.py
@@ -1,21 +1,27 @@
 import re
 import requests
 import numpy
-import cv2 # run opencv_install.sh to install
+import cv2  # run opencv_install.sh to install
+from picamera import PiCamera
+from PIL import Image
+from random import randint
+
 
 def bad_image(img):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    hist = cv2.calcHist([hsv],[1],None,[5],[0,180])
+    hist = cv2.calcHist([hsv], [1], None, [5], [0, 180])
     return min(hist) / max(hist) < 0.001
+
 
 def unskewed_image(img, box_warped, box_straight):
     img[np.where(img == 0)] = 1
     height = img.shape[0]
     width = img.shape[1]
     TM = cv2.getPerspectiveTransform(box_warped, box_straight)
-    warped = cv2.warpPerspective(img, TM, (img.shape[1],img.shape[0]))
+    warped = cv2.warpPerspective(img, TM, (img.shape[1], img.shape[0]))
     warped[np.where(warped == 0)] = img[np.where(warped == 0)]
     return warped
+
 
 def readRtspImage(scale_cam, trials=5):
     address = scale_cam["url"]
@@ -35,7 +41,8 @@ def readRtspImage(scale_cam, trials=5):
     while True:
         try:
             ret, frame = vcap.read()
-            if not bad_image(frame) or timer.read() > 5 : break
+            if not bad_image(frame) or timer.read() > 5:
+                break
         except:
             print(f"readRtspImage. Failed to capture image at {address}")
             break
@@ -46,12 +53,13 @@ def readRtspImage(scale_cam, trials=5):
     width = frame.shape[1]
     frame = unskewed_image(frame, box_from, box_to)
     cropped = frame[
-        int(height*crop_ratio[0]):int(height*crop_ratio[1]), 
+        int(height*crop_ratio[0]):int(height*crop_ratio[1]),
         int(width*crop_ratio[2]):int(width*crop_ratio[3])
     ]
     if DEBUG_WITH_DUMMY_PLATES:
         cropped = cv2.imread(DUMMY_IMG_FRONT)
     return cropped
+
 
 def chooseBestFromAPI(input_array):
     exclusions = ['mb533', 'wb533']
@@ -70,8 +78,10 @@ def chooseBestFromAPI(input_array):
             return item.upper()
     return result
 
+
 def recognizePlate(img):
-    if img is None: return ""
+    if img is None:
+        return ""
     result = ""
     headers = {'Authorization': ALPR_API_TOKEN}
     is_success, img_buffer = cv2.imencode(".jpg", img)
@@ -90,39 +100,41 @@ def isThisEmptyBox(image):
     height = image.shape[0]
     width = image.shape[1]
     img = image[
-        int(height*0.3):int(height*0.7), 
+        int(height*0.3):int(height*0.7),
         int(width*0.3):int(width*0.7)
     ]
     hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     avg_color = numpy.average(numpy.average(hsv, axis=0), axis=0)[0]
-    hist = cv2.calcHist([hsv],[0],None,[5],[0,180])
-    # print(hist)
+    hist = cv2.calcHist([hsv], [0], None, [5], [0, 255])
+    print(hist)
     ratio = (min(hist) / max(hist))[0]
     print(ratio)
     return (ratio < 0.005)
 
-loc = '/Users/Valera/Documents/venprojs/pi/latest/html/invoice_red1.jpg'
-test_img = cv2.imread(loc)
-print(loc)
-print(isThisEmptyBox(test_img))
 
-loc = '/Users/Valera/Documents/venprojs/pi/latest/html/invoice_red2.jpeg'
-test_img = cv2.imread(loc)
-print(loc)
-print(isThisEmptyBox(test_img))
+# return true if invoice photo was success
+def captureInvoiceToFile(img_file=""):
+    result = True
+    with PiCamera() as camera:
+        light_on()
+        camera.start_preview()
+        time.sleep(2)
+        camera.capture(img_file)
+        time.sleep(0.5)
+        img = cv2.imread(img_file)
+        result = not isThisEmptyBox(img)
+        cv2.imwrite(img_file, img)
+        light_off()
+    return result
 
-loc = '/Users/Valera/Documents/venprojs/pi/latest/html/invoice_ok1.jpg'
-test_img = cv2.imread(loc)
-print(loc)
-print(isThisEmptyBox(test_img))
-
-loc = '/Users/Valera/Documents/venprojs/pi/latest/html/invoice_ok2.jpg'
-test_img = cv2.imread(loc)
-print(loc)
-print(isThisEmptyBox(test_img))
+# loc = '/Users/Valera/Documents/venprojs/pi/latest/html/invoice_red1.jpg'
+# test_img = cv2.imread(loc)
+# print(loc)
+# print(isThisEmptyBox(test_img))
 
 
-loc = '/Users/Valera/Documents/venprojs/pi/latest/html/invoice_ok3.jpg'
-test_img = cv2.imread(loc)
-print(loc)
-print(isThisEmptyBox(test_img))
+value = randint(0, 100)
+
+IMAGES_DIRECTORY = '/var/www/html/'
+TEMP_INVOICE_IMG_FILE = IMAGES_DIRECTORY + f"invoice_tst_{value}.jpg"
+print(captureInvoiceToFile(img_file=TEMP_INVOICE_IMG_FILE))
