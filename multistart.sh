1bdeface187f5de232f87c07372505bb50eaa00e
Valeriy Fyodorov <valeriy.fyodorov@gmail.com>
2020-08-12 07:52:35 +0300

    Multiprocess sh

---
+++ multistart.sh
@@ -0,0 +1,8 @@
+cd ~/Desktop/flask
+export FLASK_APP=start
+export FLASK_ENV=development
+export WERKZEUG_DEBUG_PIN=off
+./start.sh run &
+# cd ~/Desktop/scales
+# ./chromium.sh
+~/Desktop/scales/chromium.sh
